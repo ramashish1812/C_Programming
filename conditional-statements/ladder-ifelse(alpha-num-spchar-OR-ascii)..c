ff476b839ebaeba90854cd91f7496d2483f2daef
ramashish1812 <ahram1807@gmail.com>
2024-09-19 10:33:59 +0530

    Add files via upload

---
+++ conditional-statements/ladder-ifelse(alpha-num-spchar-OR-ascii)..c
@@ -0,0 +1,25 @@
+//Ladder-ifelse(alpha-num-spchar-OR(||)-ascii)
+#include<stdio.h>
+int main()
+{
+	char ch;
+	
+	printf("\nEnter the value of ch = ");
+	scanf(" %c",&ch);
+	
+	if((ch>=65 && ch<=90) || (ch>=97 && ch<=122))
+	{
+		printf("\n%c is an alphabet",ch);
+	}
+	else if(ch>=48 && ch<=57)
+	{
+		printf("\nInput is a number");
+	}
+	else
+	{
+		printf("\n%c is a special character",ch);
+	}
+	
+	return 0;
+}
+
